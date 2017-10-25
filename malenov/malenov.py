63c3c0219f7cec28bd5a87a46d61ce9858d4c8f8
Philipp Lang <plang@slb.com>
2017-10-25 11:59:38 +0100

    merged d647

--- malenov/malenov.py
+++ malenov/malenov.py
@@ -5,7 +5,6 @@
 ### For: ConocoPhillips, Norway,
 ### GNU V3.0 lesser license
 
-
 # Make initial package imports
 import segyio
 import random
@@ -998,8 +997,15 @@ def predicting(filename,inp_seis,seis_obj,keras_model,cube_incr,num_classes,inp_
         # Save the numpy file
         np.save(savename + '.npy', prediction)
 
-        # Save the segy file using the input filename as a framework
-        input_file=filename
+        # Get the right filename in case the input is given as a list
+        if type(filename) is list:
+            # Save the segy file using the input filename as a framework
+            # Just use the first member of the list as the reference
+            input_file = filename[0]
+        else:
+            # Save the segy file using the input filename as a framework
+            input_file=filename
+
         output_file=savename + '.sgy'
 
         copyfile(input_file, output_file)
@@ -1479,34 +1485,34 @@ def master(segy_filename,inp_format,cube_incr,train_dict={},pred_dict={},mode = 
 
 def main():
     #### ---- Run an instance of the master function ----
-    filenames=['file1.segy','file2.segy','F3_entire.segy']    # name of the segy-cube(s) with data
+    filenames=['PGS16902VIK_pstm_ang08_17_dec_crop','PGS16902VIK_pstm_ang17_27_dec_crop', 'PGS16902VIK_pstm_ang27_33_dec_crop']    # name of the segy-cube(s) with data
     inp_res = np.float32    # formatting of the input seismic (e.g. np.int8 for 8-bit data, np.float32 for 32-bit data, etc)
     cube_incr = 32    # number of increments in each direction to create a training cube
 
     # Define the dictionary holding all the training parameters
     train_dict = {
-        'files' : ['multi_salt_ilxl.pts','multi_else_ilxl.pts', 'trainset_3', 'trainset_n'],    # list of names of class-adresses
-        'num_tot_iterations': 2,    # number of times we draw a new training ensemble/mini-batch
-        'epochs' : 2,    # number of epochs we run on each training ensemble/mini-batch
-        'num_train_ex' : 5000,    # number of training examples in each training ensemble/mini-batch
+        'files' : ['multi_channel_new.pts','multi_coherent.pts','multi_else.pts','multi_fault.pts','multi_grid.pts','multi_grizzly.pts'],    # list of names of class-adresses
+        'num_tot_iterations': 25,    # number of times we draw a new training ensemble/mini-batch
+        'epochs' : 12,    # number of epochs we run on each training ensemble/mini-batch
+        'num_train_ex' : 18000,    # number of training examples in each training ensemble/mini-batch
         'batch_size' : 32,    # number of training examples fed to the optimizer as a batch
         'opt_patience' : 10,    # number of epochs with the same accuracy before force breaking the training ensemble/mini-batch
         'data_augmentation' : False,    # whether or not we are using data augmentation
         'save_model' : True,    # whether or not we are saving the trained model
-        'save_location' : 'F3_train.h5'    # file name for the saved trained model
+        'save_location' : 'F3_train'    # file name for the saved trained model
     }
 
     # Define the dictionary holding all the prediction parameters
     pred_dict = {
-        'keras_model' :  keras.models.load_model('F3_train.h5'), # input model to be used for prediction, to load a model use: keras.models.load_model('write_location')
-        'section_edge' : np.asarray([454, 470, 454, 470, 454, 966]), # inline and xline section to be predicted (all depths), must contain xline
+        'keras_model' :  keras.models.load_model('mulitvolume_multifacies.h5'), # input model to be used for prediction, to load a model use: keras.models.load_model('write_location')
+        'section_edge' : np.asarray([33282, 33282, 123898, 123900, 128, 2840]), # inline and xline section to be predicted (all depths), must contain xline
         'show_feature' : False,    # Show the distinct features before they are combined to a prediction
-        'xline' : 458,    # xline used for classification (index)(should be within section range)
+        'xline' : 123900,    # xline used for classification (index)(should be within section range)
         'num_class' : len(train_dict['files']),    # number of classes to output
         'cord_syst' : 'segy',    # Coordinate system used, default is 0,0. Set to 'segy' to give inputs in (inline,xline)
         'save_pred' : True,    # Save the prediction as a segy-cube
-        'save_location' : 'F3_pred.h5',     # file name for the saved prediction
-        'pred_batch' : 50,     # number of traces used to make batches of mini-cubes that are stored in memory at once
+        'save_location' : 'sunday.segy',     # file name for the saved prediction
+        'pred_batch' : 25,     # number of traces used to make batches of mini-cubes that are stored in memory at once
         #'pred_batch' : train_dict['num_train_ex']//(pred_dict['section_edge'][5]-pred_dict['section_edge'][4])    #Suggested value
         'pred_prob' : False     # Give the probabilities of the first class(True), or simply show where each class is classified(False)
     }
@@ -1519,7 +1525,7 @@ def main():
         cube_incr = cube_incr,     # Increments in each direction to create a training cube
         train_dict = train_dict,     # Input training dictionary
         pred_dict = pred_dict,     # Input prediction dictionary
-        mode = 'full'     # Input mode ('train', 'predict', or 'full' for both training AND prediction)
+        mode = 'predict'     # Input mode ('train', 'predict', or 'full' for both training AND prediction)
     )
 
 
