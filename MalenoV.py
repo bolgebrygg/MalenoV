280dfc7ab3b6861d40096cbc7de3433431b768a1
Charles Rutherford Ildstad <charles.rutherford93@gmail.com>
2018-04-27 17:30:39 +0200

    Hyperparameter changes
    Made changes to fit the openly available F3 dataset

--- MalenoV.py
+++ MalenoV.py
@@ -27,7 +27,7 @@ np.random.seed(7)
 
 
 #### ---- Run an instance of the master function ----
-filenames=['F3_entire.segy']    # name of the segy-cube(s) with data
+filenames = ['F3_entire.segy']    # name of the segy-cube(s) with data
 inp_res = np.float32    # formatting of the input seismic (e.g. np.int8 for 8-bit data, np.float32 for 32-bit data, etc)
 cube_incr = 32    # number of increments in each direction to create a training cube
 
@@ -38,26 +38,26 @@ train_dict = {
                './class_addresses/multi_low_amp_dips_ilxl.pts','./class_addresses/multi_low_amplitude_ilxl.pts',
                './class_addresses/multi_low_coherency_ilxl.pts','./class_addresses/multi_salt_ilxl.pts',
                './class_addresses/multi_steep_dips_ilxl.pts'], # list of names of class-adresses
-    'num_tot_iterations': 25,    # number of times we draw a new training ensemble/mini-batch
-    'epochs' : 12,    # number of epochs we run on each training ensemble/mini-batch
+    'num_tot_iterations': 1,    # number of times we draw a new training ensemble/mini-batch
+    'epochs' : 10,    # number of epochs we run on each training ensemble/mini-batch
     'num_train_ex' : 18000,    # number of training examples in each training ensemble/mini-batch
     'batch_size' : 32,    # number of training examples fed to the optimizer as a batch
     'opt_patience' : 10,    # number of epochs with the same accuracy before force breaking the training ensemble/mini-batch
     'data_augmentation' : False,    # whether or not we are using data augmentation
     'save_model' : True,    # whether or not we are saving the trained model
-    'save_location' : 'F3_train'    # file name for the saved trained model
+    'save_location' : 'F3_test1'    # file name for the saved trained model
 }
 
 # Define the dictionary holding all the prediction parameters
 pred_dict = {
-    'keras_model' :  keras.models.load_model('mulitvolume_multifacies.h5'), # input model to be used for prediction, to load a model use: keras.models.load_model('write_location')
-    'section_edge' : np.asarray([33282, 33282, 123898, 123900, 128, 2840]), # inline and xline section to be predicted (all depths), must contain xline
+    'keras_model' :  None, # input model to be used for prediction, to load a model use: keras.models.load_model('write_location')
+    'section_edge' : np.asarray([130, 720, 775, 775, 124, 1728]), # inline and xline section to be predicted (all depths), must contain xline
     'show_feature' : False,    # Show the distinct features before they are combined to a prediction
-    'xline' : 123900,    # xline used for classification (index)(should be within section range)
+    'xline' : 775,    # xline used for classification (index)(should be within section range)
     'num_class' : len(train_dict['files']),    # number of classes to output
     'cord_syst' : 'segy',    # Coordinate system used, default is 0,0. Set to 'segy' to give inputs in (inline,xline)
-    'save_pred' : True,    # Save the prediction as a segy-cube
-    'save_location' : 'sunday.segy',     # file name for the saved prediction
+    'save_pred' : False,    # Save the prediction as a segy-cube
+    'save_location' : 'test_prediction.segy',     # file name for the saved prediction
     'pred_batch' : 25,     # number of traces used to make batches of mini-cubes that are stored in memory at once
     #'pred_batch' : train_dict['num_train_ex']//(pred_dict['section_edge'][5]-pred_dict['section_edge'][4])    #Suggested value
     'pred_prob' : False     # Give the probabilities of the first class(True), or simply show where each class is classified(False)
@@ -71,7 +71,7 @@ output_dict1 = master(
     cube_incr = cube_incr,     # Increments in each direction to create a training cube
     train_dict = train_dict,     # Input training dictionary
     pred_dict = pred_dict,     # Input prediction dictionary
-    mode = 'predict'     # Input mode ('train', 'predict', or 'full' for both training AND prediction)
+    mode = 'full'     # Input mode ('train', 'predict', or 'full' for both training AND prediction)
 )
 
 
